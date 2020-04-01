4ec5fc313570cad44a1c4eed0e3120e8f8f087d4
Cecilia Barasa <cecibarasa@gmail.com>
2020-04-01 11:31:21 +0300

    add jquery

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//UserInterface Logic
+$(document).ready(function(){
+    $(".design-showing").click(function() {
+      $(".design-hidden").toggle();
+      $(".design-hidden").toggle();
+    });
+  });
+    $(document).ready(function(){
+    $(".design-hidden").click(function() {
+      $(".design-showing").toggle();
+      $(".design-hidden").toggle();
+    });
+  });
