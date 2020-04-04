3dd90563daf4f34d063ffbd71f2af6f224a399f8
Cecilia Barasa <cecibarasa@gmail.com>
2020-04-04 08:38:58 +0300

    add final commit

--- js/script.js
+++ js/script.js
@@ -1,48 +1,48 @@
 $(function () {
-    let desp = $('#desp');
-    let devp = $('#devp');
-    let prop = $('#prop');
-    let destext = $('#destxt');
-    let devtext = $('#devtext');
-    let protext = $('#protext');
+    let design = $('#design');
+    let develop = $('#develop');
+    let product = $('#product');
+    let destxt = $('#destxt');
+    let devtxt = $('#devtxt');
+    let protxt = $('#protxt');
 
-    $(desp).hide();
-    $(devp).hide();
-    $(prop).hide();
+    $(design).hide();
+    $(develop).hide();
+    $(product).hide();
 
     //Show paragraph content
     $('.des').on('click', function () {
-        $('.des').slideUp('slow');
-        $(desp).show(500);
-        $(destext).animate({fontWeight:'bolder',fontSize:'30px'})
+        $('.des').slideToggle('slow');
+        $(design).slideToggle(500);
+        $(destxt).animate({fontWeight:'bolder',fontSize:'30px'})
 
     });
     $('.dev').on('click', function () {
-        $('.dev').slideUp('slow');
-        $(devp).show(500);
-        $(devtext).animate({fontWeight:'bolder',fontSize:'30px'})
+        $('.dev').slideToggle('slow');
+        $(develop).slideToggle(500);
+        $(devtxt).animate({fontWeight:'bolder',fontSize:'30px'})
     });
     $('.pro').on('click', function () {
         $('.pro').slideUp('slow');
-        $(prop).show(500);
-        $(protext).animate({fontWeight:'bolder',fontSize:'30px'})
+        $(product).show(500);
+        $(prodtxt).animate({fontWeight:'bolder',fontSize:'30px'})
     });
 
     //Hide paragraph content
-    $(desp, destext).on('click', function () {
+    $(design, destxt).on('click', function () {
         $('.des').slideDown();
-        $(desp).hide(500);
-        $(destext).animate({fontWeight:'normal',fontSize:'16px'})
+        $(design).hide(500);
+        $(destxt).animate({fontWeight:'normal',fontSize:'16px'})
     });
-    $(devp, devtext).on('click', function () {
-        $('.dev').slideDown();
-        $(devp).hide(500);
-        $(devtext).animate({fontWeight:'normal',fontSize:'16px'})
+    $(develop, devtxt).on('click', function () {
+        $('.dev').slideToggle();
+        $(develop).slideToggle(500);
+        $(devtxt).animate({fontWeight:'normal',fontSize:'16px'})
     });
-    $(prop, protext).on('click', function () {
-        $('.pro').slideDown();
-        $(prop).hide(500);
-        $(protext).animate({fontWeight:'normal',fontSize:'16px'})
+    $(product, prodtxt).on('click', function () {
+        $('.pro').slideToggle();
+        $(product).slideToggle(500);
+        $(prodtxt).animate({fontWeight:'normal',fontSize:'16px'})
     })
 
     //Overlay images
